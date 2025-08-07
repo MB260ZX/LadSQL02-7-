536227e04213e3ed1352b8766b30fcbda00bcfe1
MB260ZX <masterthaworawat@gmail.com>
2025-08-07 08:56:38 +0700

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+SELECT *FROM Products WHERE UnitPrice = 15;
+
+SELECT *FROM Products WHERE UnitsInStock < 250;
+
+SELECT ProductID, ProductName FROM Products
+
+SELECT ProductID,ProductName,UnitPrice from Products
+where UnitPrice >100;
