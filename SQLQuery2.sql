dcf50a62be6032fcaac13ad89f10450375bf3b68
MB260ZX <masterthaworawat@gmail.com>
2025-08-07 09:31:48 +0700

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,8 +0,0 @@
-SELECT *FROM Products WHERE UnitPrice = 15;
-
-SELECT *FROM Products WHERE UnitsInStock < 250;
-
-SELECT ProductID, ProductName FROM Products
-
-SELECT ProductID,ProductName,UnitPrice from Products
-where UnitPrice >100;
